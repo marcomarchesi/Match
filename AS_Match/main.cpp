9d6d9f09a4511ad3c4926af1123b9691ccc4c755
marcomarchesi <marchesimarco@gmail.com>
2015-11-06 00:17:27 +0100

    added populate_vector()

--- AS_Match/main.cpp
+++ AS_Match/main.cpp
@@ -48,12 +48,31 @@ void set(int* arr, int i,int j,int value)
     arr[i * edge_size + j] = value;
 }
 
-void populate_array(int* arr, vector<KeyPoint> vec)
+void populate_array(int* arr, const vector<KeyPoint>& vec)
 {
     int index = 1;
     for (int i = 0; i < vec.size(); ++i)
     {
         set(arr,vec[i].x,vec[i].y,index++);
+    }
+}
+
+void populate_vector(const int* arr,vector<KeyPoint>& vec)
+{
+    int index = 1;
+    //for now, just focus on 5 points
+    while(index < 6)
+    {
+        for(int i = 0; i < edge_size * edge_size;++i)
+        {
+            if(arr[i] == index)
+            {
+                int x = i / edge_size;
+                int y = i % edge_size;
+                vec.push_back(KeyPoint(x,y));
+                index++;
+            }
+        }
     }
 }
 
@@ -103,17 +122,37 @@ void Order(int* arr)
     }
 }
 
+void Copy(int* src_arr,int* dst_arr)
+{
+    for(int i = 0; i< edge_size * edge_size;++i)
+    {
+        dst_arr[i] = src_arr[i];
+    }
+}
+
 
 int Match(const vector<KeyPoint>& a, const vector<KeyPoint>& b,const float* rotations,float* translations)
 {
+    int sa[edge_size * edge_size];
+    int ka[edge_size * edge_size];
+    
+    init(sa);
+    init(ka);
+    
+    populate_array(sa, a);
+    populate_array(ka, b);
+    
+    
     return 0;
 };
 
 int main(int argc, const char * argv[]) {
     
     vector<KeyPoint> s_vec;
+    vector<KeyPoint> k_vec;
     int s_array[edge_size * edge_size];
     int k_array[edge_size * edge_size];
+    int t_array[edge_size * edge_size];
     
     // sample features
     s_vec.push_back(KeyPoint(3 * factor,2 * factor));
@@ -125,16 +164,21 @@ int main(int argc, const char * argv[]) {
     populate_array(s_array, s_vec);
     Order(s_array);
     // output data
-    print(s_array);
+//    printf("S points\n");
+//    print(s_array);
+    
+    Copy(s_array,k_array);
     //rotate
     float yaw = ToRadians(30.0f);
-    printf("Rotate by %.2f\n",yaw);
-    Rotate(s_array,yaw);
-    print(s_array);
+    printf("K points - We know are rotated by %.2f\n",yaw);
+    Rotate(k_array,yaw);
+    print(k_array);
+    printf("Let's verify\n");
+    // getting ready for matching algorithm
+    populate_vector(k_array, k_vec);
+    init(t_array);
+    populate_array(t_array, k_vec);
+    print(t_array);
 
-    
-    
-    
-    
     return 0;
 }
