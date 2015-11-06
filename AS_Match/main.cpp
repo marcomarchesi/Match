021e621edc7a41e2c0198955ac02453eee46a7cd
marcomarchesi <marchesimarco@gmail.com>
2015-11-06 01:15:16 +0100

    Match() first part

--- AS_Match/main.cpp
+++ AS_Match/main.cpp
@@ -15,10 +15,8 @@
 
 using namespace std;
 
-static const int factor = 1;
+static const int factor = 2;
 static const int edge_size = 10 * factor;
-
-
 
 
 class KeyPoint{
@@ -59,18 +57,16 @@ void populate_array(int* arr, const vector<KeyPoint>& vec)
 
 void populate_vector(const int* arr,vector<KeyPoint>& vec)
 {
-    int index = 1;
     //for now, just focus on 5 points
-    while(index < 6)
+    for(int k = 1; k < 6;++k)
     {
         for(int i = 0; i < edge_size * edge_size;++i)
         {
-            if(arr[i] == index)
+            if(arr[i] == k)
             {
                 int x = i / edge_size;
                 int y = i % edge_size;
                 vec.push_back(KeyPoint(x,y));
-                index++;
             }
         }
     }
@@ -86,6 +82,29 @@ void print(int* arr)
                 printf(".");
             else
                 printf("%i",arr[i * edge_size + j]);
+        }
+        printf("\n");
+    }
+}
+
+void printTwo(int* a,int* b)
+{
+    for(int i = 0;i < edge_size; ++i)
+    {
+        for (int j = 0;j < edge_size; ++j)
+        {
+            if(a[i * edge_size + j] == -1)
+                printf(".");
+            else
+                printf("%i",a[i * edge_size + j]);
+        }
+        printf("      ");
+        for (int j = 0;j < edge_size; ++j)
+        {
+            if(b[i * edge_size + j] == -1)
+                printf(".");
+            else
+                printf("%i",b[i * edge_size + j]);
         }
         printf("\n");
     }
@@ -137,12 +156,31 @@ int Match(const vector<KeyPoint>& a, const vector<KeyPoint>& b,const float* rota
     int ka[edge_size * edge_size];
     
     init(sa);
-    init(ka);
-    
     populate_array(sa, a);
+    Order(sa);
+//    print(sa);
+    
+//    printf("xxxxxxxxxxxxxxx\n");
+    init(ka);
     populate_array(ka, b);
+    Rotate(ka, -rotations[2]);
+    Order(ka);
     
+    printTwo(sa,ka);
     
+    // TRANSLATION MATCHING
+    int max_ab = (int)max(a.size(), b.size());
+    for (int i = 0;i < max_ab; ++i)
+    {
+        translations[0] += b[i].x - a[i].x;
+        translations[1] += b[i].y - a[i].y;
+        translations[2] += 0;
+    }
+    translations[0] /= max_ab;
+    translations[1] /= max_ab;
+    
+    printf("tx = %.2f and ty = %.2f\n",translations[0],translations[1]);
+        
     return 0;
 };
 
@@ -152,7 +190,8 @@ int main(int argc, const char * argv[]) {
     vector<KeyPoint> k_vec;
     int s_array[edge_size * edge_size];
     int k_array[edge_size * edge_size];
-    int t_array[edge_size * edge_size];
+    float rotations[3];
+    float translations[3];
     
     // sample features
     s_vec.push_back(KeyPoint(3 * factor,2 * factor));
@@ -164,21 +203,17 @@ int main(int argc, const char * argv[]) {
     populate_array(s_array, s_vec);
     Order(s_array);
     // output data
-//    printf("S points\n");
-//    print(s_array);
     
     Copy(s_array,k_array);
     //rotate
-    float yaw = ToRadians(30.0f);
-    printf("K points - We know are rotated by %.2f\n",yaw);
+    float yaw = ToRadians(23.0f);
+    rotations[2] = yaw;
     Rotate(k_array,yaw);
-    print(k_array);
-    printf("Let's verify\n");
+    printTwo(s_array,k_array);
+    printf("Let's match\n");
     // getting ready for matching algorithm
     populate_vector(k_array, k_vec);
-    init(t_array);
-    populate_array(t_array, k_vec);
-    print(t_array);
+    Match(s_vec, k_vec, rotations, translations);
 
     return 0;
 }
